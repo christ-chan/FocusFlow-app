38ee8b5a4540468ec1d5e2000cafe6913464052e
Your Name <your.email@example.com>
2026-07-03 18:02:39 +0000

    make schedule front

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import FocusFlowApp from './FocusFlowApp';
+import SchedulingApp from './SchedulingApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FocusFlowApp />);
+root.render(<SchedulingApp />);
